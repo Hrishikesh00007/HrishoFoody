438c34fe65afbc36fe5e67853d63bbfb440d9dc0
Hrishikesh00007 <dashrishikesh525@gmail.com>
2026-01-24 16:18:33 +0530

    Updated backend and frontend features

--- backend/controllers/item.controllers.js
+++ backend/controllers/item.controllers.js
@@ -2,43 +2,56 @@ import Item from "../models/item.model.js";
 import Shop from "../models/shop.model.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 
-export const addItem = async (req,res) => {
+export const addItem = async (req, res) => {
     try {
-         const {name,category,foodType,price} = req.body
+        const { name, category, foodType, price } = req.body
         let image;
-        if(req.file){
+        if (req.file) {
             image = await uploadOnCloudinary(req.file.path)
         }
-        const shop = await Shop.findOne({owner:req.userId})
-        if(!shop){
-            return res.status(400).json({message:`shop not found`})
+        const shop = await Shop.findOne({ owner: req.userId })
+        if (!shop) {
+            return res.status(400).json({ message: "shop not found" })
         }
         const item = await Item.create({
-            name,category,foodType,price,image,shop:shop._id
+            name, category, foodType, price, image, shop: shop._id
         })
-         return res.status(201).json(item)
+
+        shop.items.push(item._id)
+        await shop.save()
+        await shop.populate("owner")
+        await shop.populate({
+            path: "items",
+            options: { sort: { updatedAt: -1 } }
+        })
+        return res.status(201).json(shop)
+
     } catch (error) {
-         return res.status(400).json({message:`create item error ${error}`})
+        return res.status(500).json({ message: `add item error ${error}` })
     }
 }
 
-export const editItem = async (req,res) => {
+export const editItem = async (req, res) => {
     try {
         const itemId = req.params.itemId
-        const {name,category,foodType,price} = req.body
-         let image;
-        if(req.file){
+        const { name, category, foodType, price } = req.body
+        let image;
+        if (req.file) {
             image = await uploadOnCloudinary(req.file.path)
         }
-        const item = await Item.findByIdAndUpdate(itemId,{
-            name,category,foodType,price,image
-        },{new:true})
-        if(!item){
-             return res.status(400).json({message:`Item not found`})
+        const item = await Item.findByIdAndUpdate(itemId, {
+            name, category, foodType, price, image
+        }, { new: true })
+        if (!item) {
+            return res.status(400).json({ message: "item not found" })
         }
-         return res.status(201).json(item)
+        const shop = await Shop.findOne({ owner: req.userId }).populate({
+            path: "items",
+            options: { sort: { updatedAt: -1 } }
+        })
+        return res.status(200).json(shop)
 
     } catch (error) {
-         return res.status(400).json({message:`create item error ${error}`})
+        return res.status(500).json({ message: `edit item error ${error}` })
     }
 }
