318f79901f3ec1f1c7685eece7014719f50353f0
Hrishikesh00007 <dashrishikesh525@gmail.com>
2026-01-28 17:25:25 +0530

    Updated backend and frontend features

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -12,7 +12,7 @@ import { TbReceipt2 } from "react-icons/tb";
 import { useNavigate } from 'react-router-dom';
 
 function Nav() {
-        const { userData, city} = useSelector(state => state.user)
+        const { userData, currentCity,cartItems} = useSelector(state => state.user)
          const { myShopData} = useSelector(state => state.owner)
     const [showInfo, setShowInfo] = useState(false)
     const [showSearch, setShowSearch] = useState(false)
@@ -33,7 +33,7 @@ function Nav() {
             {showSearch && userData.role == "user" && <div className='w-[90%] h-[70px]  bg-white shadow-xl rounded-lg items-center gap-[20px] flex fixed top-[80px] left-[5%] md:hidden'>
                 <div className='flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400'>
                     <FaLocationDot size={25} className=" text-[#ff4d2d]" />
-                    <div className='w-[80%] truncate text-gray-600'>{city}</div>
+                    <div className='w-[80%] truncate text-gray-600'>{currentCity}</div>
                 </div>
                 <div className='w-[80%] flex items-center gap-[10px]'>
                     <IoIosSearch size={25} className='text-[#ff4d2d]' />
@@ -47,7 +47,7 @@ function Nav() {
             {userData.role == "user" && <div className='md:w-[60%] lg:w-[40%] h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px] hidden md:flex'>
                 <div className='flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px] border-gray-400'>
                     <FaLocationDot size={25} className=" text-[#ff4d2d]" />
-                    <div className='w-[80%] truncate text-gray-600'>{city}</div>
+                    <div className='w-[80%] truncate text-gray-600'>{currentCity}</div>
                 </div>
                 <div className='w-[80%] flex items-center gap-[10px]'>
                     <IoIosSearch size={25} className='text-[#ff4d2d]' />
@@ -80,7 +80,7 @@ function Nav() {
                                <>
                             {userData.role=="user" &&    <div className='relative cursor-pointer' onClick={()=>navigate("/cart")}>
                                <FiShoppingCart size={25} className='text-[#ff4d2d]' />
-                               <span className='absolute right-[-9px] top-[-12px] text-[#ff4d2d]'>{cartItems.length}</span>
+                               <span className='absolute right-[-9px] top-[-12px] text-[#ff4d2d]'>{cartItems?.length || 0}</span>
                            </div>}   
                       
            
